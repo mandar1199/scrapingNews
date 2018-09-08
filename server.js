8d4e876598a1ef3326d8dfe4be93d77269ba8da3
amanda <amandaferraro41@gmail.com>
2018-09-07 23:00:10 -0400

    morehandlebars

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 
 //PORT 5000
@@ -29,6 +30,18 @@ app.use(bodyParser.urlencoded({
 
 //every request goes through router middleware
 app.use(router);
+
+//connecting to mongodb to use database
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//connecting mongoose to our database
+mongoose.connect(db, function(error) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("mongoose connection is successful");
+    }
+});
 
 //listen on port
 app.listen(PORT, function() {
